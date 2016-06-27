2b4ac5c9773423c71d89ef05c985274aaaa10d64
陈丰尧 <734481463@qq.com>
2016-06-27 14:28:45 +0800

    把Android艺术探索里的 ImageLoader源码复制过来了，再尝试修改

---
+++ flashnet/src/main/java/com/lanou/chenfengyao/flashnet/image/temp/ImageLoader.java
@@ -0,0 +1,439 @@
+package com.lanou.chenfengyao.flashnet.image.temp;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.os.StatFs;
+import android.support.v4.util.LruCache;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.lanou.chenfengyao.flashnet.image.cache.DiskLruCache;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by zyyoona7 on 2015/12/23.
+ */
+public class ImageLoader {
+    private static final String TAG = "ImageLoader";
+
+    public static final int MESSAGE_POST_RESULT = 1;
+    //cpu核心数
+    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+    //核心线程数
+    private static final int CORE_POOL_SIZE = CPU_COUNT + 1;
+    //线程池最大容量
+    private static final int MAXIMUM_POOL_SIZE = CPU_COUNT * 2 + 1;
+    //线程闲置存活时长
+    private static final long KEEP_ALIVE = 10L;
+
+    private static final int TAG_KEY_URI = 0x7777777;
+    private static final long DISK_CACHE_SIZE = 1024 * 1024 * 50;
+    private static final int IO_BUFFER_SIZE = 8 * 1024;
+    private static final int DISK_CACHE_INDEX = 0;
+    private boolean mIsDiskLruCacheCreated = false;
+
+    private static final ThreadFactory sThreadFactory = new ThreadFactory() {
+        private final AtomicInteger mCount = new AtomicInteger(1);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "imageLoader#" + mCount.getAndIncrement());
+        }
+    };
+
+    //创建线程池
+    private static final Executor THREAD_POOL_EXECUTOR = new ThreadPoolExecutor(CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), sThreadFactory);
+
+    //初始化handler在ui线程更新界面
+    private Handler mMainHandler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message msg) {
+            LoaderResult loaderResult = (LoaderResult) msg.obj;
+            ImageView imageView = loaderResult.imageView;
+            imageView.setImageBitmap(loaderResult.bitmap);
+            String uri = (String) imageView.getTag(TAG_KEY_URI);
+            if (uri.equals(loaderResult.uri)) {
+                imageView.setImageBitmap(loaderResult.bitmap);
+            } else {
+                Log.w(TAG, "set image bitmap,but url has changed,ignored!");
+            }
+        }
+    };
+    private Context mContext;
+    private DiskLruCache mDiskLruCache;
+    private ImageResizer mImageResizer = new ImageResizer();
+    private LruCache<String, Bitmap> mMemoryCache;
+
+    private ImageLoader(Context context) {
+        mContext = context.getApplicationContext();
+        //设置内存缓存的大小
+        int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        int cacheSize = maxMemory / 8;
+        //初始化LruCache
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            /**
+             * 这个方法用于计算每个条目的大小，子类必须得复写这个类
+             * @param key
+             * @param bitmap
+             * @return
+             */
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                return bitmap.getRowBytes() * bitmap.getHeight() / 1024;
+            }
+        };
+
+        File diskCacheDir = getDiskCacheDir(mContext, "bitmap");
+        if (!diskCacheDir.exists()) {
+            diskCacheDir.mkdirs();
+        }
+
+        //如果可用空间大于设定的大小再初始化mDiskLruCache
+        if (getUsableSpace(diskCacheDir) > DISK_CACHE_SIZE) {
+            try {
+                mDiskLruCache = DiskLruCache.open(diskCacheDir, 1, 1, DISK_CACHE_SIZE);
+                mIsDiskLruCacheCreated = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 创建imageLoader对象
+     *
+     * @param context
+     * @return
+     */
+    public static ImageLoader build(Context context) {
+        return new ImageLoader(context);
+    }
+
+    /**
+     * 添加图片到内存缓存中
+     *
+     * @param key
+     * @param bitmap
+     */
+    private void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemoryCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    /**
+     * 获取内存缓存中的图片
+     *
+     * @param key
+     * @return
+     */
+    private Bitmap getBitmapFromMemoryCache(String key) {
+        return mMemoryCache.get(key);
+    }
+
+    public void bindBitmap(String uri, ImageView imageView) {
+        bindBitmap(uri, imageView, 0, 0);
+    }
+
+    /**
+     * 异步加载图片
+     *
+     * @param uri
+     * @param imageView
+     * @param reqWidth
+     * @param reqHeight
+     */
+    public void bindBitmap(final String uri, final ImageView imageView, final int reqWidth, final int reqHeight) {
+        imageView.setTag(TAG_KEY_URI, uri);
+        Bitmap bitmap = loadBitmapFromMemoryCache(uri);
+        if (bitmap != null) {
+            imageView.setImageBitmap(bitmap);
+            return;
+        }
+
+        Runnable loadBitmapTask = new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap1 = loadBitmap(uri, reqWidth, reqHeight);
+                if (bitmap1 != null) {
+                    LoaderResult result = new LoaderResult(imageView, uri, bitmap1);
+                    mMainHandler.obtainMessage(MESSAGE_POST_RESULT, result).sendToTarget();
+                }
+            }
+        };
+
+        THREAD_POOL_EXECUTOR.execute(loadBitmapTask);
+    }
+
+    /**
+     * 同步加载图片
+     *
+     * @param uri
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public Bitmap loadBitmap(String uri, int reqWidth, int reqHeight) {
+        Bitmap bitmap = loadBitmapFromMemoryCache(uri);
+        if (bitmap != null) {
+            Log.d(TAG, "loadBitmapFromMemoryCache,url:" + uri);
+            return bitmap;
+        }
+
+        try {
+            bitmap = loadBitmapFromDiskCache(uri, reqWidth, reqHeight);
+            if (bitmap != null) {
+                Log.d(TAG, "loadBitmapFromDiskCache,url:" + uri);
+                return bitmap;
+            }
+            bitmap = loadBitmapFromHttp(uri, reqWidth, reqHeight);
+            Log.d(TAG, "loadBitmapFromHttp,url:" + uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (bitmap == null && !mIsDiskLruCacheCreated) {
+            Log.w(TAG, "encounter error, DiskLruCache is not created.");
+            bitmap = downloadBitmapFromUrl(uri);
+        }
+
+        return bitmap;
+    }
+
+    /**
+     * 从内存缓存中加载图片
+     *
+     * @param url
+     * @return
+     */
+    private Bitmap loadBitmapFromMemoryCache(String url) {
+        final String key = hashKeyFormUrl(url);
+        Bitmap bitmap = getBitmapFromMemoryCache(key);
+        return bitmap;
+    }
+
+    /**
+     * 从网络拉取图片并存入存储缓存
+     *
+     * @param url
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     * @throws IOException
+     */
+    private Bitmap loadBitmapFromHttp(String url, int reqWidth, int reqHeight) throws IOException {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            throw new RuntimeException("can not visit network from UI Thread.");
+        }
+        if (mDiskLruCache == null) {
+            return null;
+        }
+
+        String key = hashKeyFormUrl(url);
+        DiskLruCache.Editor editor = mDiskLruCache.edit(key);
+        if (editor != null) {
+            OutputStream outputStream = editor.newOutputStream(DISK_CACHE_INDEX);
+            if (downloadUrlToStream(url, outputStream)) {
+                editor.commit();
+            } else {
+                editor.abort();
+            }
+            mDiskLruCache.flush();
+        }
+
+        return loadBitmapFromDiskCache(url, reqWidth, reqHeight);
+    }
+
+    /**
+     * 从存储缓存中加载bitmap
+     *
+     * @param url
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     * @throws IOException
+     */
+    private Bitmap loadBitmapFromDiskCache(String url, int reqWidth, int reqHeight) throws IOException {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            Log.w(TAG, "load bitmap from UI Thread, it's not recommended!");
+        }
+        if (mDiskLruCache == null) {
+            return null;
+        }
+
+        Bitmap bitmap = null;
+        String key = hashKeyFormUrl(url);
+        DiskLruCache.Snapshot snapshot = mDiskLruCache.get(key);
+        if (snapshot != null) {
+            FileInputStream fileInputStream = (FileInputStream) snapshot.getInputStream(DISK_CACHE_INDEX);
+            FileDescriptor fileDescriptor = fileInputStream.getFD();
+            bitmap = mImageResizer.decodeSampledBitmapFromFileDescriptor(fileDescriptor, reqWidth, reqHeight);
+            if (bitmap != null) {
+                addBitmapToMemoryCache(key, bitmap);
+            }
+        }
+        return bitmap;
+    }
+
+    /**
+     * 将下载链接转换成输出流
+     *
+     * @param urlString
+     * @param outputStream
+     * @return
+     */
+    public boolean downloadUrlToStream(String urlString, OutputStream outputStream) {
+        HttpURLConnection urlConnection = null;
+        BufferedInputStream in = null;
+        BufferedOutputStream out = null;
+
+        try {
+            final URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream(), IO_BUFFER_SIZE);
+            out = new BufferedOutputStream(outputStream, IO_BUFFER_SIZE);
+            int b;
+            while ((b = in.read()) != -1) {
+                out.write(b);
+            }
+            return true;
+        } catch (IOException e) {
+            Log.e(TAG, "downloadBitmap failed.: " + e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            MyUtils.close(in);
+            MyUtils.close(out);
+        }
+        return false;
+    }
+
+    /**
+     * 连接下载bitmap
+     *
+     * @param urlString
+     * @return
+     */
+    private Bitmap downloadBitmapFromUrl(String urlString) {
+        Bitmap bitmap = null;
+        HttpURLConnection urlConnection = null;
+        BufferedInputStream in = null;
+
+        try {
+            final URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream(), IO_BUFFER_SIZE);
+            bitmap = BitmapFactory.decodeStream(in);
+        } catch (IOException e) {
+            Log.e(TAG, "Error in downloadBitmap: " + e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            MyUtils.close(in);
+        }
+        return bitmap;
+    }
+
+    /**
+     * 将url转成md5字符串
+     *
+     * @param url
+     * @return
+     */
+    private String hashKeyFormUrl(String url) {
+        String cacheKey;
+        try {
+            final MessageDigest mDigest = MessageDigest.getInstance("MD5");
+            mDigest.update(url.getBytes());
+            cacheKey = bytesToHexString(mDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            cacheKey = String.valueOf(url.hashCode());
+        }
+        return cacheKey;
+    }
+
+    private String bytesToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(0xFF & bytes[i]);
+            if (hex.length() == 1) {
+                sb.append('0');
+            }
+            sb.append(hex);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 获取sd卡文件夹
+     *
+     * @param context
+     * @param uniqueName
+     * @return
+     */
+    public File getDiskCacheDir(Context context, String uniqueName) {
+        boolean externalStorageAvailable = Environment
+                .getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        final String cachePath;
+        if (externalStorageAvailable) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+
+        return new File(cachePath + File.separator + uniqueName);
+    }
+
+    /**
+     * 获取缓存文件夹的可用空间
+     *
+     * @param path
+     * @return
+     */
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    private long getUsableSpace(File path) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.GINGERBREAD) {
+            return path.getUsableSpace();
+        }
+        final StatFs stats = new StatFs(path.getPath());
+        return (long) stats.getBlockSize() * (long) stats.getAvailableBlocks();
+    }
+
+    private static class LoaderResult {
+        public ImageView imageView;
+        public String uri;
+        public Bitmap bitmap;
+
+        public LoaderResult(ImageView imageView, String uri, Bitmap bitmap) {
+            this.imageView = imageView;
+            this.uri = uri;
+            this.bitmap = bitmap;
+        }
+    }
+}
---
+++ flashnet/src/main/java/com/lanou/chenfengyao/flashnet/image/temp/ImageResizer.java
@@ -0,0 +1,95 @@
+package com.lanou.chenfengyao.flashnet.image.temp;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+
+import java.io.FileDescriptor;
+
+/**
+ * Created by zyyoona7 on 2015/12/23.
+ */
+public class ImageResizer {
+    private static final String TAG = "ImageResizer";
+
+    public ImageResizer() {
+
+    }
+
+    /**
+     * 加载Resources内图片的压缩方法
+     *
+     * @param resource
+     * @param resId
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public Bitmap decodeSampledBitmapFromResource(Resources resource, int resId, int reqWidth, int reqHeight) {
+        //第一步，将inJustDecodeBounds设置为true并加载图片
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(resource, resId, options);
+
+        //第二步，calculateInSampleSize方法中获取bitmap原始宽高信息，
+        // 第三步，根据需求的大小获取inSampleSize采样率
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        //第四步，将inJustDecodeBounds设置为false，重新加载图片
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(resource, resId, options);
+    }
+
+    /**
+     * 加载文件中的图片的压缩方法
+     *
+     * @param fileDescriptor
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public Bitmap decodeSampledBitmapFromFileDescriptor(FileDescriptor fileDescriptor, int reqWidth, int reqHeight) {
+        //依然是上个方法中的四步
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFileDescriptor(fileDescriptor, null, options);
+    }
+
+    /**
+     * 计算inSampleSize
+     *
+     * @param options
+     * @param reqWidth
+     * @param reqHeight
+     * @return
+     */
+    public int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        if (reqWidth == 0 || reqHeight == 0) {
+            return 1;
+        }
+
+        //获取图片的宽高
+        final int width = options.outWidth;
+        final int height = options.outHeight;
+        //初始化inSampleSize
+        int inSampleSize = 1;
+
+        //计算最大inSampleSize值是2的幂，同时保持width,height大于reqWidth,reHeight
+        if (height > reqHeight || width > reqWidth) {
+            final int halfWidth = width / 2;
+            final int halfHeight = height / 2;
+
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+        Log.d(TAG, "inSampleSize:" + inSampleSize);
+        return inSampleSize;
+    }
+}
